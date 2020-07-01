4d709a7244d051aa32f915f119c697562912c09a
john kazakopoulos <johnpkaza@gmail.com>
2020-07-01 20:48:52 +0300

    updated tsconfig

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Global } from '@emotion/core';
 import { globalStyles } from './styles/globalStyles';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App({ children }) {
   return (
